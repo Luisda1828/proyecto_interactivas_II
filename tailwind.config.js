45f72b2862e5116fe332326fd3b38a78e2e72b6a
josefabiorp <josefabio35@gmail.com>
2024-04-07 14:37:46 -0600

    html template&tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html'],
+  theme: {
+
+    
+    extend: { 
+
+    },
+  },
+  plugins: [],
+
+
+
+
+
+
+
+}
+
