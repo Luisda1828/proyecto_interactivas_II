f50684d21bddf529ea09c79520243a75f8b3d9ee
Luisda1828 <LUIS.VILLEGASHERRERA@ucr.ac.cr>
2024-04-27 22:10:29 -0600

    Creation of the calendar and events component(co-op with Diego)

--- react/vite-project/src/main.jsx
+++ react/vite-project/src/main.jsx
@@ -8,14 +8,26 @@ import {Header} from './assets/components/header.jsx'
 import { HeaderMobile } from './assets/components/header_mobile.jsx'
 import { Login } from './assets/components/login.jsx'
 import { Cuentanos } from './assets/components/cuentanos_de_ti.jsx'
-
+import { Eventos } from './assets/components/eventos.jsx'
+import { Calendario } from './assets/components/calendario.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     
-    <div>
-       {/*  <Header />
-        <HeaderMobile /> */}
+    <div className='m-12 flex flex-col gap-5' >
+         <Header />
+        <HeaderMobile />
       {/*   <Login /> */}
-       <Cuentanos/>
+        {/* <Cuentanos/>  */}
+         <div className=" flex flex-col  gap-5  sm:flex-row ">
+         <Calendario />
+          <div className="flex flex-col w-full overflow-y-auto  bg-sky-600  rounded-2xl p-4 gap-4 h-[36.5rem] relative">
+            <Eventos />
+            <Eventos />
+            <Eventos />
+            <Eventos />
+            <Eventos />
+            <Eventos />
+          </div>
+        </div>   
     </div>
 )
